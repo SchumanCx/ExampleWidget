aa367e1402012a85ecc9def5eeebbb4d30389c4c
Jennifer Schuman <schumancx@gmail.com>
2024-10-19 18:17:47 +0200

    hello word widget

---
+++ app/src/main/java/com/example/zealcasestudy/glance/WidgetReceiver.kt
@@ -0,0 +1,7 @@
+package com.example.zealcasestudy.glance
+
+import androidx.glance.appwidget.GlanceAppWidgetReceiver
+
+class WidgetReceiver : GlanceAppWidgetReceiver() {
+    override val glanceAppWidget = LottoWidgetApp()
+}
