6d86a73109e969f8c4dd97be552ab7efe6f90d16
Jennifer Schuman <schumancx@gmail.com>
2024-10-22 18:51:07 +0200

    formatting UI

--- app/src/main/java/com/example/zealcasestudy/glance/LottoResultsWidget.kt
+++ app/src/main/java/com/example/zealcasestudy/glance/LottoResultsWidget.kt
@@ -3,6 +3,7 @@ package com.example.zealcasestudy.glance
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.glance.GlanceModifier
@@ -11,13 +12,17 @@ import androidx.glance.background
 import androidx.glance.layout.Column
 import androidx.glance.layout.Row
 import androidx.glance.layout.Spacer
+import androidx.glance.layout.fillMaxWidth
 import androidx.glance.layout.padding
 import androidx.glance.layout.size
 import androidx.glance.text.FontWeight
 import androidx.glance.text.Text
 import androidx.glance.text.TextAlign
 import androidx.glance.text.TextStyle
+import androidx.glance.unit.ColorProvider
 import com.example.zealcasestudy.model.Lottery
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 @Composable
 fun LottoResultsWidget(data: List<Lottery>?) {
@@ -26,6 +31,7 @@ fun LottoResultsWidget(data: List<Lottery>?) {
         modifier = GlanceModifier
             .padding(16.dp)
             .background(MaterialTheme.colorScheme.background)
+            .fillMaxWidth()
     ) {
         if (data == null) {
             Text("Loading...")
@@ -35,30 +41,56 @@ fun LottoResultsWidget(data: List<Lottery>?) {
                 style = TextStyle(
                     fontSize = 24.sp,
                     fontWeight = FontWeight.Bold
-                )
+                ),
+                modifier = GlanceModifier.padding(bottom = 12.dp)
             )
-            Spacer(modifier = GlanceModifier.padding(16.dp))
 
-            Text(data[0].nextDraw.drawDate)
-            Spacer(modifier = GlanceModifier.padding(8.dp))
+            val formatter =
+                DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ssXXX") // Includes time and time zone offset
+            val dateTime = LocalDateTime.parse(data[0].lastDraw.drawDate, formatter)
+            val formattedDateTime =
+                dateTime.format(DateTimeFormatter.ofPattern("EEE, dd.MM.yyyy")) // Format for display
+            Text(
+                text = formattedDateTime,
+                style = TextStyle(
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight.Bold,
+                    color = ColorProvider(Color.DarkGray)
+                ),
+                modifier = GlanceModifier.padding(bottom = 8.dp)
+            )
 
-            Row {
-                LotteryBall(data[0].lastDraw.drawResult.numbers)
-                Spacer(modifier = GlanceModifier.padding(16.dp))
-                if (data[0].nextDraw.jackpot.jackpotSupported) {
-                    Text(data[0].nextDraw.jackpot.jackpots.WC_1)
-                }
+            LotteryBall(
+                numbers = data[0].lastDraw.drawResult.numbers,
+                bottomPadding = 16.dp
+            )
+            if (data[0].nextDraw.jackpot.jackpotSupported) {
+                Text(
+                    text = "Jackpot: " + data[0].nextDraw.jackpot.jackpots.WC_1,
+                    style = TextStyle(
+                        fontSize = 24.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = ColorProvider(Color.Red)
+                    ),
+                    modifier = GlanceModifier
+                        .background(Color.Yellow)
+                        .padding(8.dp)
+                        .cornerRadius(16.dp)
+                )
             }
         }
     }
 }
 
 @Composable
-fun LotteryBall(numbers: List<Int>) {
+fun LotteryBall(
+    numbers: List<Int>,
+    bottomPadding: Dp = 0.dp
+) {
     Row(
         modifier = GlanceModifier
-            .background(Color.LightGray)
             .padding(6.dp)
+            .background(Color.LightGray)
             .cornerRadius(100.dp)
     ) {
         numbers.forEachIndexed { index, number ->
@@ -79,4 +111,5 @@ fun LotteryBall(numbers: List<Int>) {
             }
         }
     }
+    Spacer(modifier = GlanceModifier.padding(bottomPadding))
 }
